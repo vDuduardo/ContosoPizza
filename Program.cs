b2d03f5b441366c3cd2e7288d2a07927f715f416
Eduardo Verbinen <everbinen@gmail.com>
2022-04-02 18:24:37 -0300

    Saving git token

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+//vscode://vscode.github-authentication/did-authenticate?windowid=1&code=6e59e265c6dbbde2a007&state=37bc540f-d0e5-4a91-97c0-4ca5634e443e  - Git Token
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
